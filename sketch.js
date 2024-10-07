d83975e45226a1134b54b297e0ecc038a9baf9a1
Dajohnson000 <daj22c@fsu.edu>
2024-10-07 10:44:00 -0400

    Images in it

--- sketch.js
+++ sketch.js
@@ -1,12 +1,38 @@
 
+let state = 0;
+let i = 0;
+let image = [];
+
 function setup() {
   createCanvas(400, 400);
  
 }
 function draw() {
-  background(255);
-  fill(boxGray);
-  square(width/2,height/2,boxSize);
+  background(0);
+  switch(state){
+    
+    case(0):
+
+    
+    break;
+    case(1):
+
+    
+    break;
+    case(2):
+
+    
+    break;
+    case(3):
+
+    
+    break;
+    case(4):
+
+    
+    break;
+  } 
+  
 }
 
 
