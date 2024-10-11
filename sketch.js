0c4dc66231a26b7dadee291af6312af86e371184
JAKE CHAPUT <JWC21@FSU.EDU>
2024-10-11 12:13:23 -0400

    defined chirp variable and put into preload in sketch.js file

--- sketch.js
+++ sketch.js
@@ -3,16 +3,19 @@ let state = 0;
 let i = 0;
 let images = [];
 let showAlternateImage = true;
+let chirp
+
 function preload(){
     images[0] = loadImage("libraries/Happi.png");
     images[1] = loadImage("libraries/Sad.png");
     images[2] = loadImage("libraries/Error.png");
     images[3] = loadImage("libraries/Dizzy.png");
-    images[4] = loadImage("libraries/Ded.png")
-    sounds[0] = loadSound("assets/robot getting mad.mp3")
-    sounds[1] = loadSound("assets/screaming robot mad .mp3")
-    sounds[2] = loadSound("assets/robot dizzy.mp3")
-    sounds[3] = loadSound("assets/robotdeath.mp3")
+    images[4] = loadImage("libraries/Ded.png");
+    sounds[0] = loadSound("assets/robot getting mad.mp3");
+    sounds[1] = loadSound("assets/screaming robot mad .mp3");
+    sounds[2] = loadSound("assets/robot dizzy.mp3");
+    sounds[3] = loadSound("assets/robotdeath.mp3");
+    chirp = loadSound('libraries/wake-up-the-robot-84894.mp3');
 }
 function setup() {
   createCanvas(windowWidth,windowHeight);
@@ -27,9 +30,6 @@ function draw() {
     case(0):
        if (showAlternateImage == true) {
         image(images[0], 0, 0)// show the alt image
-        if (mouseIsPressed == true){
-          chirp.play();
-        }
       } 
       else {
         (showAlternateImage = false) 
@@ -37,6 +37,11 @@ function draw() {
       }
       
       //happy to angry
+
+      if (mouseIsPressed) {
+        chirp.play();
+      }
+
     break;
     case(1):
 
