6884204c0f38bef463266bdb4123f46cc00435bb
Dajohnson000 <daj22c@fsu.edu>
2024-10-12 12:56:15 -0400

    text device shaken

--- sketch.js
+++ sketch.js
@@ -1,95 +1,72 @@
-
 let state = 0;
 let i = 0;
 let images = [];
 let showAlternateImage = true;
-let chirp
+let chirp;
+let sounds = [];
 
-function preload(){
-    images[0] = loadImage("libraries/Happi.png");
-    images[1] = loadImage("libraries/Sad.png");
-    images[2] = loadImage("libraries/Error.png");
-    images[3] = loadImage("libraries/Dizzy.png");
-    images[4] = loadImage("libraries/Ded.png");
-    sounds[0] = loadSound("assets/robot getting mad.mp3");
-    sounds[1] = loadSound("assets/screaming robot mad .mp3");
-    sounds[2] = loadSound("assets/robot dizzy.mp3");
-    sounds[3] = loadSound("assets/robotdeath.mp3");
-    chirp = loadSound('libraries/wake-up-the-robot-84894.mp3');
+function preload() {
+  images[0] = loadImage("libraries/Happi.png");
+  images[1] = loadImage("libraries/Sad.png");
+  images[2] = loadImage("libraries/Error.png");
+  images[3] = loadImage("libraries/Dizzy.png");
+  images[4] = loadImage("libraries/Ded.png");
+
 }
 function setup() {
-  createCanvas(windowWidth,windowHeight);
- 
+  createCanvas(windowWidth, windowHeight);
+  sounds[0] = loadSound("libraries/robot getting mad.mp3");
+  sounds[1] = loadSound("libraries/screaming robot mad .mp3");
+  sounds[2] = loadSound("libraries/robot dizzy.mp3");
+  sounds[3] = loadSound("libraries/robotdeath.mp3");
+  chirp = loadSound("libraries/wake-up-the-robot-84894.mp3");
+
 }
 function draw() {
-  
-  
-
-  switch(state){
-    
-    case(0):
-       if (showAlternateImage == true) {
-        image(images[0], 0, 0)// show the alt image
-      } 
-      else {
-        (showAlternateImage = false) 
-          image(images[2], 0, 0)
+  switch (state) {
+    case 0:
+      if (showAlternateImage == true) {
+        image(images[0], 0, 0); // show the alt image
+      } else {
+        showAlternateImage = false;
+        image(images[2], 0, 0);
       }
-      
+
       //happy to angry
 
-      if (mouseIsPressed) {
-        chirp.play();
+      //if (mouseIsPressed) {
+       // chirp.play();
+      //}
+
+      break;
+    case 1:
+      if (showAlternateImage == true) {
+        image(images[2], 0, 0); // show the alt image
+      } else {
+        showAlternateImage = false;
+        image(images[1], 0, 0);
       }
+      //angry to sad
+      //if (key == ".") state = state + 1;
 
-    break;
-    case(1):
-    if (showAlternateImage == true) {
-      image(images[2], 0, 0)// show the alt image
-    } 
-    else {
-      (showAlternateImage = false) 
-        image(images[1], 0, 0)
-    }
-    //angry to sad
-      if (key == ".") state = (state + 1);
+      break;
 
-    break;
-  
-   
-    case(2):
-    if (showAlternateImage == true) {
-      image(images[1], 0, 0)// show the alt image
-    } 
-    else {
-      (showAlternateImage = false) 
-        image(images[3], 0, 0)
-    }
-    //sad to dizzy
-    break;
-    case(3):
-    if (showAlternateImage == true) {
-      image(images[3], 0, 0)// show the alt image
-    } 
-    else {
-      (showAlternateImage = false) 
-        image(images[4], 0, 0)
-    }
-    // dizzy to ded
-    break;
-    case(4):
-    if (showAlternateImage == true) {
-      image(images[4], 0, 0)// show the alt image
-    }
-    //ded 
-    break;
-  } 
+    case 2:
+      if (showAlternateImage == true) {
+        image(images[1], 0, 0); // show the alt image
+      } else {
+        showAlternateImage = false;
+        image(images[3], 0, 0);
+      }
+      //sad to dizzy
+      break;
+    case 3:
+      image(images[3], 0, 0); // show the alt image
+      // dizzy to ded
+      break;
+    case 4:
+      image(images[4], 0, 0); // show the alt image
+      //ded
+      break;
+  }
 }
-
-
-
-
-
-
-
-
--- sketch2.js
+++ sketch2.js
@@ -1,4 +1,9 @@
-function keyPressed(){
+let shakes = 0;
+let shakey = 0;
+let minimumShakes = 5;
+let caseShakes = 10;
+
+function keyTyped(){
     setTimeout (function() {
     if (state == 0)
         if (key == '1'){
@@ -8,23 +13,63 @@ function keyPressed(){
 }, 2000);
 
     if (state == 1)
-    if (key == '2'){
+        if (key == '2'){
         
         showAlternateImage = showAlternateImage ? false : true; 
 }
     if (state ==2)
-    if (key == '3'){
+        if (key == '3'){
         
         showAlternateImage = showAlternateImage ? false : true; 
 }
-if (state == 3)
-    if (key == '4'){
+    if (state == 3)
+        if (key == '4'){
         
         showAlternateImage = showAlternateImage ? false : true; 
 }
-if (state == 4)
-    if (key == '5'){
+   if (state == 4)
+       if (key == '5'){
         
         showAlternateImage = showAlternateImage ? false : true; 
 }
+    if (keyCode == '13'){
+       (state = (state + 1) % 5);
+       showAlternateImage = true;
+    }
 }
+
+function deviceShaken(){
+   if (state = 2){
+    shakes++
+    shakey++
+    if (shakes <= minimumShakes){
+        showAlternateImage == true;
+    }
+    if (shakes >= minimumShakes){
+        showAlternateImage == false;
+    }
+    if (shakey >= caseShakes){
+        (state = state +1) % 5;
+        shakey = 0;
+        shakes = 0;
+    }
+}
+if (state = 3){
+
+    shakey++
+    if (shakey >= caseShakes){
+        (state = state +1) % 5;
+        shakey = 0;
+        shakes = 0;
+    }
+}
+if (state = 4){
+    shakey++
+    if (shakey >= caseShakes){
+        (state = state +1) % 5;
+        shakey = 0;
+        shakes = 0;
+    }
+}
+}
+console.log(state);
