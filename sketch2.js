fcdc350c1dbd063424ec49ff44083045dcbf15d1
Dajohnson000 <daj22c@fsu.edu>
2024-10-14 09:41:17 -0400

    Test caseShakes2

--- sketch2.js
+++ sketch2.js
@@ -2,6 +2,7 @@ let shakes = 0;
 let shakey = 0;
 let minimumShakes = 50;
 let caseShakes = 100;
+let caseShakes2 = 200;
 
 function keyPressed() {
   console.log(state);
@@ -12,47 +13,46 @@ function keyPressed() {
       showAlternateImage = showAlternateImage ? false : true;
     }
   }
-    //AUDIO KEYBINDS
-    if (key == "q") {
-      //chirp (happy)
-      sounds[0].play();
-    }
-
-    if (key == "w") {
-      //aawwuAAAWU (anger)
-      sounds[1].play();
-    }
-
-    if (key == "e") {
-      //ooooWWEAUUAUW*beep* (anger)
-      sounds[2].play();
-    }
-
-    if (key == "r") {
-      //sad oowoah oowoah oooooooowauuhh (dizzy/sad)
-      sounds[3].play();
-    }
-
-    if (key == "t") {
-      //mouse shriek (dizzy/angry)
-      sounds[4].play();
-    }
-
-    if (key == "y") {
-      sounds[5].play();
-    }
-
-    if (key == "u") {
-      sounds[6].play();
-    }
-  } //, 2000);
-
-  if (state == 1) {
-    if (key == "1") {
-      showAlternateImage = showAlternateImage ? false : true;
-    }
+  //AUDIO KEYBINDS
+  if (key == "q") {
+    //chirp (happy)
+    sounds[0].play();
   }
 
+  if (key == "w") {
+    //aawwuAAAWU (anger)
+    sounds[1].play();
+  }
+
+  if (key == "e") {
+    //ooooWWEAUUAUW*beep* (anger)
+    sounds[2].play();
+  }
+
+  if (key == "r") {
+    //sad oowoah oowoah oooooooowauuhh (dizzy/sad)
+    sounds[3].play();
+  }
+
+  if (key == "t") {
+    //mouse shriek (dizzy/angry)
+    sounds[4].play();
+  }
+
+  if (key == "y") {
+    sounds[5].play();
+  }
+
+  if (key == "u") {
+    sounds[6].play();
+  }
+} //, 2000);
+
+if (state == 1) {
+  if (key == "1") {
+    showAlternateImage = showAlternateImage ? false : true;
+  }
+}
 
 function deviceShaken() {
   console.log(state);
@@ -73,7 +73,7 @@ function deviceShaken() {
   }
 
   if (state == 3) shakey++;
-  if (shakey >= caseShakes) {
+  if (shakey >= caseShakes2) {
     state = (state + 1) % 5;
     shakes = 0;
     shakey = 0;
