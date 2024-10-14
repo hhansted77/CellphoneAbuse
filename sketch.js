813ae3b0d76a0dd4bd74cb0af87131879df62430
Dajohnson000 <daj22c@fsu.edu>
2024-10-14 10:16:29 -0400

    Remove force fullscreen

--- sketch.js
+++ sketch.js
@@ -70,12 +70,3 @@ function draw() {
       break;
   }
 }
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
